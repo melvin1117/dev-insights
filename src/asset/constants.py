6fe6071b93afe2922cbf3223ac2664045a0bef9f
SHUBHAM MELVIN FELIX <felix.melvin117@gmail.com>
2023-11-20 16:45:53 -0500

    gh data miner updated the concurrency fixes

--- src/asset/constants.py
+++ src/asset/constants.py
@@ -1,1 +1,4 @@
-# our constants goes here
+from utils.config_loader import load_config
+
+# Define a global constant by reading the JSON file
+CONFIG_DATA = load_config()
--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,6 +1,7 @@
 import json
 
+
 def load_config():
-    with open('src/config.json', 'r') as config_file:
+    with open("asset/config.json", "r") as config_file:
         config = json.load(config_file)
     return config
